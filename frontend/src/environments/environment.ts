5df30afd6345e0d56d3d0abaaa5c7fc637c261e9
gurpreetgit2 <gurpreet2b@gmail.com>
2024-12-28 16:47:46 +0530

    working_all

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -5,6 +5,6 @@ export const environment = {
     url: 'dev-h7urz8wu1psv0ct0.us', 
     audience: 'https://coffee-shop-app/api', 
     clientId: 'svTAkDp6zcCr1U1zQXQBG4qTAF0oUp38',
-    callbackURL: 'http://127.0.0.1:5001/login-results', 
+    callbackURL: 'http://127.0.0.1:8100', 
   }
 };
